ea27bd6858af2532c9adad57c23743d217654c86
Nobel Barakat <nbarakat6@gatech.edu>
2021-01-10 16:40:57 -0700

    using dwm blocks

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -13,19 +13,21 @@ feh --bg-fill ~/Wallpapers/oldest_house.jpg &
 xset r rate 279 40 &
 
 # Set status bar to display time and battery
-while true; do
-	amixer -c 3 sget Mic | rg "\[on\]"
-	MIC_IS_ON=$(echo $?)
-
-	MIC_MESSAGE="Mic is OFF"
-	if [ "$MIC_IS_ON" -eq 0 ]; then
-		MIC_MESSAGE="Mic is LIVE"
-	fi
-
-	LOAD_AVG=$(cat /proc/loadavg | awk -F ' ' '{print $1}')
-    BATT=$(acpi)
-	NETWORK=$(nmcli -t -f name connection show --active)
-
-	xsetroot -name " $NETWORK | $BATT | Load Avg: $LOAD_AVG | $MIC_MESSAGE | $(date +'%D %H:%M') "
-	sleep 1
-done &
+# while true; do
+# 	amixer -c 3 sget Mic | rg "\[on\]"
+# 	MIC_IS_ON=$(echo $?)
+# 
+# 	MIC_MESSAGE="Mic is OFF"
+# 	if [ "$MIC_IS_ON" -eq 0 ]; then
+# 		MIC_MESSAGE="Mic is LIVE"
+# 	fi
+# 
+# 	LOAD_AVG=$(cat /proc/loadavg | awk -F ' ' '{print $1}')
+#     BATT=$(acpi | cut -d' ' -f3-)
+# 
+# 	NETWORK=$(nmcli -t -f name connection show --active)
+# 
+# 	xsetroot -name " $NETWORK | $BATT | Load Avg: $LOAD_AVG | $MIC_MESSAGE | $(date +'%D %H:%M') "
+# 	sleep 1
+# done &
+dwmblocks &
