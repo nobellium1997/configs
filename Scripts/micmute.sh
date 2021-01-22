717a7679f60eb8ebf72623c24fca2db563fd2f6e
Nobel <nobarakat@gmail.com>
2021-01-22 08:04:53 -0700

    new scripts directory

---
+++ Scripts/micmute.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+amixer -c3 get Mic | rg "\[on\]" >/dev/null
+MIC_IS_ON=$(echo $?)
+
+MIC_MESSAGE="Mic is OFF"
+if [ "$MIC_IS_ON" -eq 0 ]; then
+    MIC_MESSAGE="Mic is LIVE"
+fi
+
+echo $MIC_MESSAGE
---
+++ Scripts/mutemic.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+amixer set -c3 Mic toggle
