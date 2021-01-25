24551684560d8807f2861a128ff36d5ba2323cba
Nobel Barakat <nbarakat6@gatech.edu>
2021-01-24 21:43:18 -0700

    adding safer scripts

--- Scripts/getpass.sh
+++ Scripts/getpass.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
-source ~/\.passwords/bw_pass
-local NAME=$(bw list items | jq '.[].name' | dmenu)
-local NAME_STRIPPED=$(echo $NAME | sed 's/"//g')
-local PASS=$(bw get password $NAME_STRIPPED)
-echo $PASS | xclip -sel clip
+function getpass {
+    source ~/\.passwords/bw_pass
+    local NAME=$(bw list items | jq '.[].name' | dmenu)
+    local NAME_STRIPPED=$(echo $NAME | sed 's/"//g')
+    local PASS=$(bw get password $NAME_STRIPPED)
+    echo $PASS | xclip -sel clip
+}
+
+getpass
--- Scripts/getuser.sh
+++ Scripts/getuser.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
-source ~/\.passwords/bw_pass
-local NAME=$(bw list items | jq '.[].name' | dmenu)
-local NAME_STRIPPED=$(echo $NAME | sed 's/"//g')
-local USER=$(bw get username $NAME_STRIPPED)
-echo $USER | xclip -sel clip
+function getuser {
+    source ~/\.passwords/bw_pass
+    local NAME=$(bw list items | jq '.[].name' | dmenu)
+    local NAME_STRIPPED=$(echo $NAME | sed 's/"//g')
+    local USER=$(bw get username $NAME_STRIPPED)
+    echo $USER | xclip -sel clip
+}
+
+getuser
