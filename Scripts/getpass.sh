7284d8ae52073f7d2491d0bbcb0f445283913672
Nobel <nobarakat@gmail.com>
2021-01-26 22:32:10 -0700

    making bw scripts better

--- Scripts/getpass.sh
+++ Scripts/getpass.sh
@@ -2,9 +2,9 @@
 set -e
 function getpass {
     source ~/\.passwords/bw_pass
-    local NAME=$(bw list items | jq '.[].name' | dmenu -i)
+    local NAME=$(bw list items --nointeraction | jq '.[].name' | dmenu -i)
     local NAME_STRIPPED=$(echo $NAME | sed 's/"//g')
-    local PASS=$(bw get password $NAME_STRIPPED)
+    local PASS=$(bw list items --search "$NAME_STRIPPED" --nointeraction | jq '.[0].login.password' | sed 's/"//g')
     echo $PASS | xclip -sel clip
 }
 
--- Scripts/getuser.sh
+++ Scripts/getuser.sh
@@ -2,9 +2,9 @@
 set -e
 function getuser {
     source ~/\.passwords/bw_pass
-    local NAME=$(bw list items | jq '.[].name' | dmenu -i)
+    local NAME=$(bw list items --nointeraction | jq '.[].name' | dmenu -i)
     local NAME_STRIPPED=$(echo $NAME | sed 's/"//g')
-    local USER=$(bw get username $NAME_STRIPPED)
+    local USER=$(bw list items --search "$NAME_STRIPPED" --nointeraction | jq '.[0].login.username' | sed 's/"//g')
     echo $USER | xclip -sel clip
 }
 
