57992c0dfc3db801489ef629a8379ea01e697bec
Nobel <nobarakat@gmail.com>
2021-01-29 16:56:36 -0700

    new file watcher

--- watcher.sh
+++ watcher.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-inotifywait -e modify -r ~/temp -m |
-    while read path action file; do
-        echo "Transferring $file"
-        # rsync $file nobel.barakat@10.0.0.53:C:\Users\nobel.barakat\work
-    done
+function watcher {
+    inotifywait -e modify -r $1 -m \
+        --excludei "\.(dll|cache|swp|git|gitignore)" |
+        while read path action file; do
+            if [[ "$path" == *"obj"* || "$path" == *"bin"* ]]; then
+                echo "ignored"
+            else
+                echo "Transferring $path$file"
+                local sub_path=$(echo $path | sed "s:/home/nobel/Work/Services/::g")
+                scp "$path$file" nobel.barakat@10.0.0.158:C:/Users/nobel.barakat/Work/Services/"$sub_path$file"
+            fi
+
+        done
+}
+
+watcher $1
