bda364c2f2d15a9b515f6cb8694744ffda316fb6
Nobel <nbarakat6@gatech.edu>
2021-03-20 01:09:08 -0600

    new dmenu changes

--- Scripts/getpass.sh
+++ Scripts/getpass.sh
@@ -2,7 +2,7 @@
 set -e
 function getpass {
     source ~/\.passwords/bw_pass
-    local NAME=$(bw list items --nointeraction | jq '.[].name' | dmenu -i)
+    local NAME=$(bw list items --nointeraction | jq '.[].name' | dmenu -i -c -l 20 )
     local NAME_STRIPPED=$(echo $NAME | sed 's/"//g')
     local PASS=$(bw list items --search "$NAME_STRIPPED" --nointeraction | jq '.[0].login.password' | sed 's/"//g')
     echo $PASS | xclip -sel clip
