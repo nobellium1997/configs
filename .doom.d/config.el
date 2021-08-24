276b5c8fbf6d270e2df386e6f0fb44fa14c85bc1
Nobel <nobarakat@gmail.com>
2021-08-24 17:12:08 -0600

    new stuff

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -22,7 +22,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 14))
+(setq doom-font (font-spec :family "monospace" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -197,6 +197,7 @@
 
 ;; Set char mode by default
 (setq exwm-manage-configurations '((t char-mode t)))
+(setq exwm-edit-paste-delay 0.3)
 
 ;; Also shrink fringes to 1 pixel.
 (fringe-mode 1)
