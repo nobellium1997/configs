19cc975316f4811bdad3acac0376e26b46b67859
Nobel <nobarakat@gmail.com>
2021-08-26 17:44:11 -0600

    going dark

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -27,7 +27,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-acario-light)
+(setq doom-theme 'doom-dracula)
 (after! ivy
   (set-face-attribute 'ivy-current-match nil :underline t))
 
